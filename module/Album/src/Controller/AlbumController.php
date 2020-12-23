d9821f2281d4f7e63f7bd7b0ffc543786dfd89b9
Thanh <thanhnv@ecommage.com>
2020-12-23 16:28:04 +0700

    update module

--- module/Album/src/Controller/AlbumController.php
+++ module/Album/src/Controller/AlbumController.php
@@ -100,7 +100,30 @@ class AlbumController extends AbstractActionController
         return $this->redirect()->toRoute('album', ['action' => 'index']);
     }
 
+// Add content to the following method:
     public function deleteAction()
     {
+        $id = (int) $this->params()->fromRoute('id', 0);
+        if (!$id) {
+            return $this->redirect()->toRoute('album');
+        }
+
+        $request = $this->getRequest();
+        if ($request->isPost()) {
+            $del = $request->getPost('del', 'No');
+
+            if ($del == 'Yes') {
+                $id = (int) $request->getPost('id');
+                $this->table->deleteAlbum($id);
+            }
+
+            // Redirect to list of albums
+            return $this->redirect()->toRoute('album');
+        }
+
+        return [
+            'id'    => $id,
+            'album' => $this->table->getAlbum($id),
+        ];
     }
 }
